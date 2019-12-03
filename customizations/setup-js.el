38572901f25d977d21a8bcab0d5645702c57aeb4
Charles <echen902@gmail.com>
2019-12-02 20:08:52 -0800

    Re-nuked my settings

--- customizations/setup-js.el
+++
@@ -1,4 +0,0 @@
-;; javascript / html
-(add-to-list 'auto-mode-alist '("\\.js$" . rjsx-mode))
-(setq js-indent-level 2)
-(setq js2-strict-missing-semi-warning nil)
--- init.el
+++ init.el
@@ -1,159 +1,177 @@
-;;;;
-;; Packages
-;;;;
+;; -*- coding: utf-8; lexical-binding: t; -*-
+;; (advice-add #'package-initialize :after #'update-load-path)
 
-;; Define package repositories
-(require 'package)
-(add-to-list 'package-archives
-             '("tromey" . "http://tromey.com/elpa/") t)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+;; Without this comment emacs25 adds (package-initialize) here
+;; (package-initialize)
 
-(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
-(add-to-list 'package-pinned-packages '(magit . "melpa-stable") t)
+(push (expand-file-name "~/.emacs.d/lisp") load-path)
 
+(let* ((minver "24.4"))
+  (when (version< emacs-version minver)
+    (error "Emacs v%s or higher is required." minver)))
 
-;; Load and activate emacs packages. Do this first so that the
-;; packages are loaded before you start trying to modify them.
-;; This also sets the load path.
-(package-initialize)
+(defvar best-gc-cons-threshold
+  4000000
+  "Best default gc threshold value.  Should NOT be too big!")
 
-;; Download the ELPA archive description if needed.
-;; This informs Emacs about the latest versions of all packages, and
-;; makes them available for download.
-(when (not package-archive-contents)
-  (package-refresh-contents))
+;; don't GC during startup to save time
+(setq gc-cons-threshold most-positive-fixnum)
 
-;; Define he following variables to remove the compile-log warnings
-;; when defining ido-ubiquitous
-;; (defvar ido-cur-item nil)
-;; (defvar ido-default-item nil)
-;; (defvar ido-cur-list nil)
-;; (defvar predicate nil)
-;; (defvar inherit-input-method nil)
+(setq emacs-load-start-time (current-time))
 
-;; The packages you want installed. You can also install these
-;; manually with M-x package-install
-;; Add in your own as you wish:
-(defvar my-packages
-  '(;; makes handling lisp expressions much, much easier
-    ;; Cheatsheet: http://www.emacswiki.org/emacs/PareditCheatsheet
-    paredit
+;; {{ emergency security fix
+;; https://bugs.debian.org/766397
+(eval-after-load "enriched"
+  '(defun enriched-decode-display-prop (start end &optional param)
+     (list start end)))
+;; }}
+;;----------------------------------------------------------------------------
+;; Which functionality to enable (use t or nil for true and false)
+;;----------------------------------------------------------------------------
+(setq *is-a-mac* (eq system-type 'darwin))
+(setq *win64* (eq system-type 'windows-nt))
+(setq *cygwin* (eq system-type 'cygwin) )
+(setq *linux* (or (eq system-type 'gnu/linux) (eq system-type 'linux)) )
+(setq *unix* (or *linux* (eq system-type 'usg-unix-v) (eq system-type 'berkeley-unix)) )
+(setq *emacs24* (>= emacs-major-version 24))
+(setq *emacs25* (>= emacs-major-version 25))
+(setq *emacs26* (>= emacs-major-version 26))
+(setq *no-memory* (cond
+                   (*is-a-mac*
+                    (< (string-to-number (nth 1 (split-string (shell-command-to-string "sysctl hw.physmem")))) 4000000000))
+                   (*linux* nil)
+                   (t nil)))
 
-    ;; key bindings and code colorization for Clojure
-    ;; https://github.com/clojure-emacs/clojure-mode
-    clojure-mode
+;; @see https://www.reddit.com/r/emacs/comments/55ork0/is_emacs_251_noticeably_slower_than_245_on_windows/
+;; Emacs 25 does gc too frequently
+(when *emacs25*
+  ;; (setq garbage-collection-messages t) ; for debug
+  (setq best-gc-cons-threshold (* 64 1024 1024))
+  (setq gc-cons-percentage 0.5)
+  (run-with-idle-timer 5 t #'garbage-collect))
 
-    ;; extra syntax highlighting for clojure
-    clojure-mode-extra-font-locking
+(defun require-init (pkg &optional maybe-disabled)
+  "Load PKG if MAYBE-DISABLED is nil or it's nil but start up in normal slowly."
+  (when (or (not maybe-disabled) (not (boundp 'startup-now)))
+    (load (file-truename (format "~/.emacs.d/lisp/%s" pkg)) t t)))
 
-    ;; integration with a Clojure REPL
-    ;; https://github.com/clojure-emacs/cider
-    cider
+(defun local-require (pkg)
+  (unless (featurep pkg)
+    (load (expand-file-name
+           (cond
+            ((eq pkg 'go-mode-load)
+             (format "~/.emacs.d/site-lisp/go-mode/%s" pkg))
+            (t
+             (format "~/.emacs.d/site-lisp/%s/%s" pkg pkg))))
+          t t)))
 
-    ;; allow ido usage in as many contexts as possible. see
-    ;; customizations/navigation.el line 23 for a description
-    ;; of ido
-    ido-completing-read+
+;; *Message* buffer should be writable in 24.4+
+(defadvice switch-to-buffer (after switch-to-buffer-after-hack activate)
+  (if (string= "*Messages*" (buffer-name))
+      (read-only-mode -1)))
 
-    ;; Enhances M-x to allow easier execution of commands. Provides
-    ;; a filterable list of possible commands in the minibuffer
-    ;; http://www.emacswiki.org/emacs/Smex
-    smex
+;; @see https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/
+;; Normally file-name-handler-alist is set to
+;; (("\\`/[^/]*\\'" . tramp-completion-file-name-handler)
+;; ("\\`/[^/|:][^/|]*:" . tramp-file-name-handler)
+;; ("\\`/:" . file-name-non-special))
+;; Which means on every .el and .elc file loaded during start up, it has to runs those regexps against the filename.
+(let* ((file-name-handler-alist nil))
 
-    ;; project navigation
-    projectile
+  ;; ;; {{
+  ;; (require 'benchmark-init-modes)
+  ;; (require 'benchmark-init)
+  ;; (benchmark-init/activate)
+  ;; ;; `benchmark-init/show-durations-tree' to show benchmark result
+  ;; ;; }}
 
-    company
+  (require-init 'init-autoload)
+  ;; `package-initialize' takes 35% of startup time
+  ;; need check https://github.com/hlissner/doom-emacs/wiki/FAQ#how-is-dooms-startup-so-fast for solution
+  (require-init 'init-modeline)
+  (require-init 'init-utils)
+  (require-init 'init-elpa)
+  (require-init 'init-exec-path t) ;; Set up $PATH
+  ;; Any file use flyspell should be initialized after init-spelling.el
+  (require-init 'init-spelling t)
+  (require-init 'init-gui-frames t)
+  (require-init 'init-uniquify t)
+  (require-init 'init-ibuffer t)
+  (require-init 'init-ivy)
+  (require-init 'init-hippie-expand)
+  (require-init 'init-windows)
+  (require-init 'init-markdown t)
+  (require-init 'init-javascript t)
+  (require-init 'init-org t)
+  (require-init 'init-css t)
+  (require-init 'init-python t)
+  (require-init 'init-ruby-mode t)
+  (require-init 'init-lisp t)
+  (require-init 'init-clojure t)
+  (require-init 'init-clojure-cider t)
+  (require-init 'init-elisp t)
+  (require-init 'init-yasnippet t)
+  (require-init 'init-cc-mode t)
+  (require-init 'init-gud t)
+  (require-init 'init-linum-mode)
+  (require-init 'init-git t)
+  ;; (require-init 'init-gist)
+  (require-init 'init-gtags t)
+  (require-init 'init-clipboard)
+  (require-init 'init-ctags t)
+  (require-init 'init-bbdb t)
+  (require-init 'init-gnus t)
+  (require-init 'init-lua-mode t)
+  (require-init 'init-term-mode t)
+  (require-init 'init-web-mode t)
+  (require-init 'init-company t)
+  (require-init 'init-chinese t) ;; cannot be idle-required
+  ;; need statistics of keyfreq asap
+  (require-init 'init-keyfreq t)
+  (require-init 'init-httpd t)
 
-    ;; colorful parenthesis matching
-    rainbow-delimiters
+  ;; projectile costs 7% startup time
 
-    ;; jsx support
-    rjsx-mode
+  ;; misc has some crucial tools I need immediately
+  (require-init 'init-essential)
+  (require-init 'init-workgroups2)
+  (require-init 'init-misc t)
 
-    ;; git integration
-    magit))
+  (require-init 'init-emacs-w3m t)
+  (require-init 'init-shackle t)
+  (require-init 'init-dired t)
+  (require-init 'init-writting t)
+  (require-init 'init-hydra) ; hotkey is required everywhere
+  ;; use evil mode (vi key binding)
+  (require-init 'init-evil) ; init-evil dependent on init-clipboard
 
-;; On OS X, an Emacs instance started from the graphical user
-;; interface will have a different environment than a shell in a
-;; terminal window, because OS X does not run a shell during the
-;; login. Obviously this will lead to unexpected results when
-;; calling external utilities like make from Emacs.
-;; This library works around this problem by copying important
-;; environment variables from the user's shell.
-;; https://github.com/purcell/exec-path-from-shell
-(if (eq system-type 'darwin)
-    (add-to-list 'my-packages 'exec-path-from-shell))
+  ;; ediff configuration should be last so it can override
+  ;; the key bindings in previous configuration
+  (require-init 'init-ediff)
 
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
+  ;; @see https://github.com/hlissner/doom-emacs/wiki/FAQ
+  ;; Adding directories under "site-lisp/" to `load-path' slows
+  ;; down all `require' statement. So we do this at the end of startup
+  ;; NO ELPA package is dependent on "site-lisp/".
+  (setq load-path (cdr load-path))
+  (my-add-subdirs-to-load-path "~/.emacs.d/site-lisp/")
 
+  (unless (boundp 'startup-now)
+    ;; my personal setup, other major-mode specific setup need it.
+    ;; It's dependent on "~/.emacs.d/site-lisp/*.el"
+    (load (expand-file-name "~/.custom.el") t nil)
 
-;; Place downloaded elisp files in ~/.emacs.d/vendor. You'll then be able
-;; to load them.
-;;
-;; For example, if you download yaml-mode.el to ~/.emacs.d/vendor,
-;; then you can add the following code to this file:
-;;
-;; (require 'yaml-mode)
-;; (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-;;
-;; Adding this code will make Emacs enter yaml mode whenever you open
-;; a .yml file
-(add-to-list 'load-path "~/.emacs.d/vendor")
+    ;; @see https://www.reddit.com/r/emacs/comments/4q4ixw/how_to_forbid_emacs_to_touch_configuration_files/
+    ;; See `custom-file' for details.
+    (load (setq custom-file (expand-file-name "~/.emacs.d/custom-set-variables.el")) t t)))
 
+(setq gc-cons-threshold best-gc-cons-threshold)
 
-;;;;
-;; Customization
-;;;;
+(when (require 'time-date nil t)
+  (message "Emacs startup time: %d seconds."
+           (time-to-seconds (time-since emacs-load-start-time))))
 
-;; Add a directory to our load path so that when you `load` things
-;; below, Emacs knows where to look for the corresponding file.
-(add-to-list 'load-path "~/.emacs.d/customizations")
-
-;; Sets up exec-path-from-shell so that Emacs will use the correct
-;; environment variables
-(load "shell-integration.el")
-
-;; These customizations make it easier for you to navigate files,
-;; switch buffers, and choose options from the minibuffer.
-(load "navigation.el")
-
-;; These customizations change the way emacs looks and disable/enable
-;; some user interface elements
-(load "ui.el")
-
-;; These customizations make editing a bit nicer.
-(load "editing.el")
-
-;; Hard-to-categorize customizations
-(load "misc.el")
-
-;; For editing lisps
-(load "elisp-editing.el")
-
-;; Langauage-specific
-(load "setup-clojure.el")
-(load "setup-js.el")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(coffee-tab-width 2)
- '(custom-safe-themes
-   (quote
-    ("52588047a0fe3727e3cd8a90e76d7f078c9bd62c0b246324e557dfa5112e0d0c" "9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" default)))
- '(package-selected-packages
-   (quote
-    (company magit tagedit rainbow-delimiters projectile smex ido-completing-read+ cider clojure-mode-extra-font-locking clojure-mode paredit exec-path-from-shell))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;; Local Variables:
+;;; no-byte-compile: t
+;;; End:
+(put 'erase-buffer 'disabled nil)
